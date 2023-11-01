7c13e2375f77cc20ed554e79cea9815c81c4367e
Cornelius Andreas Rosenaa <75324436+coffandro@users.noreply.github.com>
2023-11-01 13:07:53 +0100

    Switched to Dialog manager and made some text

--- scenes/John.gd
+++ scenes/John.gd
@@ -1,15 +1,14 @@
 extends Node
 
-
+@onready var balloon: CanvasLayer = $Balloon
 
 func _ready():
+#	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Control/Template.play("default")
 
-func _input(event:InputEvent):
-	# check if a dialog is already running
-	if Dialogic.current_timeline != null:
-		return
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("dialogic_default_action"):
+		balloon.start(load("res://characters/John.dialogue"), "this_is_a_node_title")
+		#DialogueManager.show_example_dialogue_balloon(load("res://characters/John.dialogue"), "this_is_a_node_title")
+		get_viewport().set_input_as_handled()	
 	
-	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
-		Dialogic.start('Chapter1')
-		get_viewport().set_input_as_handled()
